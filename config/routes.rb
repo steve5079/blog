a7e3e2b0110e631fbaca7e7f3af3b33ab54cc93b
steve5079 <stephendau@gmail.com>
2016-06-07 22:28:34 +0200

    reinitialize blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  resources :articles
-    resources :comments
-end
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -59,4 +53,4 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
+end
